963a80b498c92e6437416b718da0fa9ee468d626
Manisha <singhal@atix.de>
2019-12-16 17:19:53 +0100

    Snapshot model_name modification foreman > 1.22 uses x-editable-rails-1.5.5.1 with change in model werein/x-editable-rails@1498f54#diff-cdf16e91ee71c2c9d748acb0ec2c1d2d

--- app/models/foreman_snapshot_management/snapshot.rb
+++ app/models/foreman_snapshot_management/snapshot.rb
@@ -5,6 +5,7 @@ require 'date'
 module ForemanSnapshotManagement
   class Snapshot
     extend ActiveModel::Callbacks
+    extend ActiveModel::Naming
     include ActiveModel::Conversion
     include ActiveModel::Model
     include ActiveModel::Dirty
@@ -15,6 +16,17 @@ module ForemanSnapshotManagement
     attr_writer :create_time
     attr_reader :name, :description, :include_ram, :host_id
     define_attribute_methods :name, :description, :include_ram
+
+    def self.model_name
+      Struct.new(:name, :klass, :singular, :plural, :element,
+                 :human, :collection, :param_key, :i18n_key, :route_key, :singular_route_key).new(
+                   'ForemanSnapshotManagement::Snapshot', ForemanSnapshotManagement::Snapshot,
+                   'foreman_snapshot_management_snapshot', 'foreman_snapshot_management_snapshots',
+                   'snapshot', 'Snapshot', 'foreman_snapshot_management/snapshots',
+                   'snapshot', :'foreman_snapshot_management/snapshot', 'foreman_snapshot_management_snapshots',
+                   'foreman_snapshot_management_snapshot'
+                 )
+    end
 
     def self.new_for_host(host)
       host.compute_resource.new_snapshot(host)
