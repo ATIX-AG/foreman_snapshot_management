ATIX-AG/foreman_snapshot_management

commit 9d9a8357c428a60cb3052016620218635b58a9a3

Add separate jenkins-test jobs

lib/tasks/foreman_snapshot_management_tasks.rake
@@ -10,20 +10,39 @@ namespace :test do
   end
 end
 
-namespace :foreman_snapshot_management do
-  task :rubocop do
-    begin
-      require 'rubocop/rake_task'
+begin
+  require 'rubocop/rake_task'
+
+  test_patterns = ["#{ForemanSnapshotManagement::Engine.root}/app/**/*.rb",
+                   "#{ForemanSnapshotManagement::Engine.root}/lib/**/*.rb",
+                   "#{ForemanSnapshotManagement::Engine.root}/test/**/*.rb"]
+
+  namespace :foreman_snapshot_management do
+    task :rubocop do
       RuboCop::RakeTask.new(:rubocop_foreman_snapshot_management) do |task|
-        task.patterns = ["#{ForemanSnapshotManagement::Engine.root}/app/**/*.rb",
-                         "#{ForemanSnapshotManagement::Engine.root}/lib/**/*.rb",
-                         "#{ForemanSnapshotManagement::Engine.root}/test/**/*.rb"]
+        task.patterns = test_patterns
       end
-    rescue StandardError
-      puts 'Rubocop not loaded.'
+
+      Rake::Task['rubocop_foreman_snapshot_management'].invoke
     end
 
-    Rake::Task['rubocop_foreman_snapshot_management'].invoke
+    desc 'Runs Rubocop style checker with xml output for Jenkins'
+    RuboCop::RakeTask.new('rubocop:jenkins') do |task|
+      task.patterns = test_patterns
+      task.requires = ['rubocop/formatter/checkstyle_formatter']
+      task.formatters = ['RuboCop::Formatter::CheckstyleFormatter']
+      task.options = ['--no-color', '--out', 'rubocop.xml']
+    end
+  end
+rescue LoadError
+  puts 'Rubocop not loaded.'
+end
+
+namespace :jenkins do
+  desc 'Test ForemanSnapshotManagement with XML output for jenkins'
+  task 'foreman_snapshot_management' do
+    Rake::Task['jenkins:setup:minitest'].invoke
+    Rake::Task['rake:test:foreman_snapshot_management'].invoke
   end
 end
 

commit 222c31bfdb183474087a6029d8c0e4b74729fb0b

Don't show Rubocop not loaded warning
This error is shown at runtime to users using foreman-rake. Prior to
9d9a8357c428a60cb3052016620218635b58a9a3 it was only attempting to load
RuboCop when invoking the task.

Fixes: 9d9a8357c428 ("Add separate jenkins-test jobs")

lib/tasks/foreman_snapshot_management_tasks.rake
@@ -37,7 +37,7 @@ begin
     end
   end
 rescue LoadError
-  puts 'Rubocop not loaded.'
+  # 'Rubocop not loaded.'
 end
 
 namespace :jenkins do